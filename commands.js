30277176251e40d9e8c48046c06c849d927f16c7
Troy Dorman <troydorman@gmail.com>
2021-09-08 09:58:27 -0600

    utilizing inquirer the add command now prompts the user to add the customer info

--- commands.js
+++ commands.js
@@ -1,20 +1,53 @@
 const program = require("commander");
+const { prompt } = require("inquirer");
 const { addCustomer, findCustomer } = require("./index");
+
+// customer questions
+const customerQuestions = [
+  {
+    type: "input",
+    name: "firstname",
+    message: "What is the customer's first name?",
+  },
+  {
+    type: "input",
+    name: "lastname",
+    message: "What is the customer's last name?",
+  },
+  {
+    type: "input",
+    name: "phone",
+    message: "What is the customer's phone number?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the customer's email address?",
+  },
+];
 
 program.version("0.0.1").description("CLI for managing customers");
 
-program
+/* program
   .command("add <firstname> <lastname> <phone> <email>")
   .alias("a")
   .description("Add a customer to the database")
   .action((firstname, lastname, phone, email) => {
     addCustomer({ firstname, lastname, phone, email });
+  }); */
+
+program
+  .command("add")
+  .alias("a")
+  .description("Add a customer to the database")
+  .action(() => {
+    prompt(customerQuestions).then((answers) => addCustomer(answers));
   });
 
-  program
-  .command('find <name>')
-  .alias('f')
-  .description('Find a customer in the database.')
-  .action(name => findCustomer(name));
+program
+  .command("find <name>")
+  .alias("f")
+  .description("Find a customer in the database.")
+  .action((name) => findCustomer(name));
 
 program.parse(process.argv);
