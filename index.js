77782a6afce34fda72f2fa00d50950115b77a6bd
Troy Dorman <troydorman@gmail.com>
2021-09-08 09:12:41 -0600

    installed dot env to hide db credentials.  connected to db.

---
+++ index.js
@@ -0,0 +1,16 @@
+// require dotenv
+require("dotenv").config();
+
+// require mongoose
+const mongoose = require("mongoose");
+
+// map global promises
+mongoose.Promise = global.Promise;
+
+// connect to db
+const db = mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING. {
+    useMongoClient: true
+});
+
+
+
