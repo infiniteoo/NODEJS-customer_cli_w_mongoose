42570b8a2724c14d5a85a258e4f21166fa0d11d0
Troy Dorman <troydorman@gmail.com>
2021-09-08 09:24:40 -0600

    created add command to our cli with commander

--- commands.js
+++ commands.js
@@ -3,4 +3,12 @@ const { addCustomer, findCustomer } = require("./index");
 
 program.version("0.0.1").description("CLI for managing customers");
 
+program
+  .command("add <firstname> <lastname> <phone> <email>")
+  .alias("a")
+  .description("Add a customer to the database")
+  .action((firstname, lastname, phone, email) => {
+    addCustomer({ firstname, lastname, phone, email });
+  });
+
 program.parse(process.argv);
